adfb147ca855e5a163cf0ba9f69f3ac92cfe8732
larryyu2004 <jiayiyu@myyahoo.com>
2025-05-17 21:44:41 +0930

    ignore

--- next-portfolio/git.sh
+++ next-portfolio/git.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+# Prompt the user for the file to add
+echo "Enter the path of the file to add (e.g., app/blog/[...slug]/page.tsx):"
+read git_add_file
+
+
 # Prompt the user for a commit message
 echo "Enter your commit message:"
 read commit_message
 
 # Stage all changes
-git add .
+git add "$git_add_file"
 
 # Commit with the provided message
 git commit -m "$commit_message"
