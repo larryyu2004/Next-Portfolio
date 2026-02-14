40000a88b86e134707d46eba56e0b2b888044d48
Larry Yu <jiayiyu@myyahoo.com>
2026-02-14 14:54:11 +0800

    fix: fixed the layout issue in the blog page

--- next-portfolio/app/blog/[...slug]/page.tsx
+++ next-portfolio/app/blog/[...slug]/page.tsx
@@ -32,7 +32,7 @@ export default async function Page({ params }: PageProps) {
         <div className="pt-[25px] xl:pt-[80px] px-2 sm:px-4 py-8 w-full h-full box-border">
           <div className="flex flex-col xl:flex-row gap-6 max-w-[1920px] mx-auto h-full">
             {/* Main Content */}
-            <div className="w-full xl:w-4/5 xl:h-[calc(100vh-140px)] xl:overflow-y-scroll bg-white dark:bg-slate-900/80 rounded-2xl shadow-xl border border-gray-100 dark:border-slate-700/60 backdrop-blur-sm scroll-smooth custom-scrollbar scroll-pt-24">
+            <div className="w-full xl:w-4/5 xl:h-[calc(100vh-100px)] xl:overflow-y-scroll bg-white dark:bg-slate-900/80 rounded-2xl shadow-xl border border-gray-100 dark:border-slate-700/60 backdrop-blur-sm scroll-smooth custom-scrollbar scroll-pt-24">
               <div className="p-6 ">
                 <article className="prose prose-lg dark:prose-invert max-w-none">
                   <Post />
